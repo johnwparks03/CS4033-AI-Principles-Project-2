3a5e383044f3e175e4c7edc70bcf8f1b28b4f09c
John Parks <johnwparks03@gmail.com>
2024-04-09 13:27:48 -0400

    completed q1

--- tracking/inference.py
+++ tracking/inference.py
@@ -61,7 +61,41 @@ def constructBayesNet(gameState: hunters.GameState):
     variableDomainsDict = {}
 
     "*** YOUR CODE HERE ***"
-    raiseNotDefined()
+    boardPositions = []
+    observations = []
+
+    variables.append(PAC)
+    variables.append(GHOST0)
+    variables.append(GHOST1)
+    variables.append(OBS0)
+    variables.append(OBS1)
+
+    edges.append((GHOST0, OBS0))
+    edges.append((PAC, OBS0))
+    edges.append((PAC, OBS1))
+    edges.append((GHOST1, OBS1))
+
+    for x in range(X_RANGE):
+        for y in range(Y_RANGE):
+            boardPositions.append((x, y))
+
+    for boardPosition in boardPositions:
+        manhattanDistance = boardPosition[0] + boardPosition[1]
+        manDistMinusNoise = manhattanDistance - MAX_NOISE
+        manDistPlusNoise = manhattanDistance + MAX_NOISE
+        if manDistMinusNoise >= 0 and manDistMinusNoise not in observations:
+            observations.append(manDistMinusNoise)
+        if manDistPlusNoise not in observations:
+            observations.append(manDistPlusNoise)
+
+
+    variableDomainsDict[PAC] = boardPositions
+    variableDomainsDict[GHOST0] = boardPositions
+    variableDomainsDict[GHOST1] = boardPositions
+    variableDomainsDict[OBS0] = observations
+    variableDomainsDict[OBS1] = observations
+
+    bayesNet = bn.constructEmptyBayesNet(variables, edges, variableDomainsDict)
     "*** END YOUR CODE HERE ***"
 
     net = bn.constructEmptyBayesNet(variables, edges, variableDomainsDict)
